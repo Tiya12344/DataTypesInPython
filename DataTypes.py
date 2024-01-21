de317f7e38052c007c91fe8bcea3643bf863e52e
Tiya Noonia <tiyanoonia2901@gmail.com>
2024-01-21 18:25:52 +0000

    i have written the code mentioning the data type in python

---
+++ DataTypes.py
@@ -0,0 +1,14 @@
+age = 12
+print (age,type(age))
+
+peopleInMillion = 3.4
+print (peopleInMillion,type(peopleInMillion))
+
+x = 31j
+print (x,type(x))
+
+foodlist = ["Maggi","chocolate","Hotdogs"]
+print(foodlist,type(foodlist))
+
+foodlist1 = ("Maggi","chocolate","Hotdogs")
+print(foodlist1,type(foodlist1))
